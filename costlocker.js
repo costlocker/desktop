3d2c8c68b7a9d7e0b4a62925a971ffb57ec78f1f
Zdeněk Drahoš <drahoszdenek@gmail.com>
2018-07-09 14:55:51 +0200

    Costlocker - refactoring sending event

--- costlocker.js
+++ costlocker.js
@@ -1,16 +1,15 @@
 const { ipcRenderer } = require('electron');
 
-const reloadApp = (state) => ipcRenderer.sendToHost(state);
-const reloadAppTimeout = (state, seconds) => ipcRenderer.sendToHost(state, seconds);
+const reloadApp = (state, data) => ipcRenderer.sendToHost(state, data);
 
 // Costlocker -> Desktop
 global._clWrapApp = {
-    setWindowSettings_: (settings) => reloadAppTimeout('update-window', settings),
-    setRunningEntry_: (settings) => reloadAppTimeout('update-tray', settings),
-    startDetectingIdleTime_: (seconds) => reloadAppTimeout('update-idletime', seconds),
-    stopDetectingIdleTime: () => reloadAppTimeout('update-idletime', null),
-    startRemindingToTrack_: (seconds) => reloadAppTimeout('update-reminder', seconds),
-    stopRemindingToTrack: () => reloadAppTimeout('update-reminder', null),
+    setWindowSettings_: (settings) => reloadApp('update-window', settings),
+    setRunningEntry_: (settings) => reloadApp('update-tray', settings),
+    startDetectingIdleTime_: (seconds) => reloadApp('update-idletime', seconds),
+    stopDetectingIdleTime: () => reloadApp('update-idletime', null),
+    startRemindingToTrack_: (seconds) => reloadApp('update-reminder', seconds),
+    stopRemindingToTrack: () => reloadApp('update-reminder', null),
     presentPopover_: () => reloadApp('app-show'),
     dismissPopover: () => reloadApp('app-hide'),
     terminateApp: () => reloadApp('app-quit'),
