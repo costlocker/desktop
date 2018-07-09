852b6b4f91af722461b446d521b522f313f9db9d
Zdeněk Drahoš <drahoszdenek@gmail.com>
2018-07-09 14:24:58 +0200

    Costlocker - refactoring tray integration

--- costlocker.js
+++ costlocker.js
@@ -1,33 +1,11 @@
 const { ipcRenderer } = require('electron');
 
-let traySettings = {
-    isActive: false,
-    name: '',
-    timestamp: '',
-};
-let trayTimeout = null;
-
-const reloadTray = (updatedSettings) => {
-    traySettings = { ...traySettings, ...updatedSettings };
-    if (trayTimeout) {
-        return;
-    }
-    trayTimeout = setTimeout(
-        () => {
-            ipcRenderer.sendToHost('update-tray', traySettings);
-            trayTimeout = clearTimeout(trayTimeout);
-        },
-        100
-    );
-};
 const reloadApp = (state) => ipcRenderer.sendToHost(state);
 const reloadAppTimeout = (state, seconds) => ipcRenderer.sendToHost(state, seconds);
 
 // Costlocker -> Desktop
 global._clWrapApp = {
-    setStatusBarIcon_: (isActive) => reloadTray({ isActive }),
-    setProjectName_: (name) => reloadTray({ name }),
-    setProjectTime_: (timestamp) => reloadTray({ timestamp }),
+    setRunningEntry_: (settings) => reloadAppTimeout('update-tray', settings),
     startDetectingIdleTime_: (seconds) => reloadAppTimeout('update-idletime', seconds),
     stopDetectingIdleTime: () => reloadAppTimeout('update-idletime', null),
     startRemindingToTrack_: (seconds) => reloadAppTimeout('update-reminder', seconds),
